08691abbf5933b8d568536854f05e08d34af41a1
mk-hasan <mkhasan6891@gmail.com>
2022-05-20 21:48:09 +0600

    project method loading without slots

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 }); //project add for without slots
             const services = await cursor.toArray();
             res.send(services);
         });
